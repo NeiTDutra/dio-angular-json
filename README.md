75c6fd6d29a1e02b1013b86c6399c605360adb76
nei <nei.thomass@gmail.com>
2021-09-17 14:53:13 -0300

    envia esqueleto do projeto

---
+++ README.md
@@ -0,0 +1,11 @@
+# Blog Json
+
+*Projeto de estudos - Bootcamp Capgemini Full Stack Java Angular - Digital Innovation One Inc.*
+
+## Escopo do produto
+
+TODO...
+
+## Escopo do projeto
+
+TODO...
---
+++ frontend/dio-blog/src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { FeedComponent } from './feed/feed.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'feed',
+    component: FeedComponent
+  },
+  {
+    path: 'contato',
+    component: ContactComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
